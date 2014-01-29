42904d43fe99fabdfa8dd507e4ccdd0b4b10dc93
Nad Flores <nflores@appsource.biz>
2014-01-29 14:20:04 +0800

    add admin index and news layout

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   resources :subproject_implementation_plans
   resources :subprojects
   resources :request_for_fund_releases
+
+  scope '/admin' do
+    get '/index', to: 'admin#index', as: 'admin_index'
+    get '/news', to: 'admin#news', as: 'admin_news'
+  end
 end
