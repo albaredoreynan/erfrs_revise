e06613f21503f1c7190ed16ecd8908d757c73f1d
Kenneth Lindsey Calamay <git@kennethcalamay.com>
2014-01-31 14:33:57 +0800

    Adds logic for parsing philippine barangays

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+def region(row)
+  @region ||= begin
+                code, name = row['region'].split('-')
+                Region.find_or_create_by(code: code.strip, name: name.strip)
+              end
+end
+
+def province(row)
+  @province ||= begin
+                  name = row['province']
+                  region(row).provinces.find_or_create_by(name: name.strip)
+                end
+end
+
+def municipality(row)
+  @municipality ||= begin
+                      name = row['city']
+                      province(row).municipalities.find_or_create_by(name: name.strip)
+                    end
+end
+
+def parse_barangay(row)
+  name = row['name']
+  municipality(row).barangays.find_or_create_by(name: name.strip)
+end
+
+
+file = File.read Rails.root.join('db/seeds/philippine-barangays.csv')
+csv = CSV.parse(file, :headers => true)
+
+csv.each do |row|
+  puts "parsing #{row['name']}, #{row['city']}"
+  parse_barangay(row)
+  [:@region, :@province, :@municipality].each &method(:remove_instance_variable)
+end
+
+
