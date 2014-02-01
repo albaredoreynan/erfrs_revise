3ffe1534b8e90aed80e804059db3a081ac216359
Kenneth Lindsey Calamay <git@kennethcalamay.com>
2014-02-01 11:26:00 +0800

    Subproject scopes

--- app/models/subproject.rb
+++ app/models/subproject.rb
@@ -9,4 +9,29 @@ class Subproject < ActiveRecord::Base
   PHYSICAL_TARGETS = %w{Target1 Target2}
   COST_PARAMETERS = %w{Parameter1 Parameter2}
   MODES_OF_IMPLEMENTATION = %w{Mode1 Mode2}
+
+  ####################### SCOPES ###########################
+
+  scope :by_user,     -> username { fetch_all_created_by username }
+  scope :with_id,     -> id { where id: id }
+  scope :with_status, -> status { where status: status }
+
+  if ENV['ERFRS_USES_POSTGRESQL']
+    scope :with_year, -> year { where 'EXTRACT(YEAR FROM created_at) = ?', year }
+  else
+    scope :with_year, -> year { where 'YEAR(created_at) = ?', year  }
+  end
+
+  %w{region province municipality barangay}.each do |place|
+    scope "from_#{place}".intern, -> place_id { where "#{place}_id" => place_id }
+  end
+
+  ####################### SCOPES ###########################
+
+  private
+
+  def self.fetch_all_created_by(username)
+    users = User.where 'username like ?', "%#{username}%"
+    user.any? ? where( user_id: users.pluck(:id) ) : none
+  end
 end
