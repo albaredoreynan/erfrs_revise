365defa326633fc9d45e3c51870dab3de78e2c9c
Kenneth Lindsey Calamay <git@kennethcalamay.com>
2014-02-01 20:49:44 +0800

    Integrates filtering logic with forms

--- app/controllers/subprojects_controller.rb
+++ app/controllers/subprojects_controller.rb
@@ -1,7 +1,7 @@
 class SubprojectsController < ApplicationController
 
   %w[id user year status].each{ |e| has_scope "with_#{e}".intern }
-  %w[region province municipality barangay].each{ |e| has_scope "from_#{e}".intern }
+  %w[region province municipality barangay].each{ |e| has_scope "subproject_#{e}_id".intern }
 
   def index
     @subprojects = apply_scopes(Subproject).includes(
