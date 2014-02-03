31fb33704b251a5d56757089283f22bc76ae385c
Nad Flores <nflores@appsource.biz>
2014-02-03 23:21:50 +0800

    super_user can create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,24 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'User was successfully created.'
+      redirect_to users_path
+    else
+      flash[:error] = 'An error occured while creating new user.'
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :username, :first_name, :last_name, :password, :password_confirmation)
+    end
+
 end
