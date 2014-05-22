74bfa4f0c3b6b0046bf1e3d525a0f8112186b9b5
Nad Flores <nflores@appsource.biz>
2014-05-22 10:47:59 +0800

    remove super_user links

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
   has_many :subprojects
 end
--- config/routes.rb
+++ config/routes.rb
@@ -26,5 +26,4 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users
-  devise_for :super_users
 end
