5a7c4425c8804dd2411e42bdcec394691aeacf2b
Reynan Albaredo <chezca1228@yahoo.com>
2014-06-05 18:37:22 +0800

    deployment tool

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,7 @@ set :deploy_to, '/home/azureuser'
 set :user, :azureuser
 set :nginx_server_name, 'dswd-erfrs.cloudapp.net'
 
-set :linked_files, %w[config/database.yml tmp/pids/unicorn.pid log/unicorn.log]
+# set :linked_files, %w[config/database.yml tmp/pids/unicorn.pid log/unicorn.log]
 
 server 'dswd-erfrs.cloudapp.net', user: 'azureuser', roles: %w[web app]
 
