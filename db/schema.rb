c9a8cbe371e1dcea090b2355fbdb1a08bf4ca677
Nad Flores <nflores@appsource.biz>
2014-06-17 20:27:23 +0800

    merge conflict on Gemfile.lock

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,6 @@ ActiveRecord::Schema.define(version: 20140612175038) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "approval_informations", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "barangays", force: true do |t|
     t.integer  "municipality_id"
     t.string   "name"
@@ -57,11 +52,6 @@ ActiveRecord::Schema.define(version: 20140612175038) do
     t.datetime "updated_at"
     t.integer  "fund_source_id"
     t.integer  "year"
-  end
-
-  create_table "fund_informations", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "fund_sources", force: true do |t|
