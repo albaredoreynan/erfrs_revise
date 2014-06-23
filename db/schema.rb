34fe6df67d2e8f2a3a2fb0b8fd12028bdb70af7b
Reynan Albaredo <chezca1228@yahoo.com>
2014-06-23 14:44:53 +0800

    changes on scripts

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,11 @@ ActiveRecord::Schema.define(version: 20140620065348) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "approval_informations", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "barangays", force: true do |t|
     t.integer  "municipality_id"
     t.string   "name"
@@ -54,6 +59,11 @@ ActiveRecord::Schema.define(version: 20140620065348) do
     t.integer  "year"
   end
 
+  create_table "fund_informations", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "fund_sources", force: true do |t|
     t.string   "code"
     t.string   "name"
@@ -83,6 +93,8 @@ ActiveRecord::Schema.define(version: 20140620065348) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "group_id"
+    t.decimal  "grant_allocation", precision: 15, scale: 2, default: 0.0, null: false
+    t.integer  "year"
   end
 
   add_index "municipalities", ["group_id"], name: "index_municipalities_on_group_id", using: :btree
