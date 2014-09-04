3178bcec1cb5191f9347d3d26a3a5645d5531a05
Reynan Albaredo <chezca1228@yahoo.com>
2014-09-04 12:06:04 +0800

    fixes for rfr pdfs and validation

--- app/models/request_for_fund_release.rb
+++ app/models/request_for_fund_release.rb
@@ -4,10 +4,9 @@ class RequestForFundRelease < ActiveRecord::Base
   # TODO fix relationship to disregard requested_by_first_designation
   # belongs_to :requested_by_first, class_name: 'TeamMember', foreign_key: "requested_by_first"
 
-
-  validates_presence_of :obr_number, :if => :confirm_presence_of_obr_date?, :message => "OBR Date Missing"
-  validates_presence_of :obr_date, :if => :confirm_presence_of_obr_number?, :message => "OBR Number Missing"
-
+  # changes for Sept 03, 2014
+  # validates_presence_of :obr_number, :if => :confirm_presence_of_obr_date?, :message => "OBR Date Missing"
+  # validates_presence_of :obr_date, :if => :confirm_presence_of_obr_number?, :message => "OBR Number Missing"
 
   %w{region province municipality barangay}.each do |place|
     scope "#{place}_id".intern, -> place_id { includes(:subproject).where "subprojects.#{place}_id" => place_id }
