50e536585a0aaf1021ae25a97034bac061e2c24d
Nad Flores <nflores@appsource.biz>
2014-09-08 13:21:09 +0800

    add recent display to news and information in dashboard

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,13 +2,14 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :landing_page
 
   def dashboard
+    @news_informations = NewsInformation.where("publish_start <= ? and publish_end >= ?", Date.today, Date.today ).recent
   end
 
   def landing_page
     if current_user
-     redirect_to dashboard_path  
+      redirect_to dashboard_path  
     end
   end
 
-  
+
 end
