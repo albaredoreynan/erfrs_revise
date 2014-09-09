9cb0a740900909739455b571a3cba7ac038a2737
Ezra Valmores <evalmores@appsource.biz>
2014-09-09 15:35:37 +0800

    removed default scope

--- app/models/region.rb
+++ app/models/region.rb
@@ -3,8 +3,6 @@ class Region < ActiveRecord::Base
   has_many :provinces
   has_many :regional_officers
 
-  default_scope -> { order('name ASC') }
-
   def self.values_for_select
     Region.all.map{ |r| [r.to_s, r.id]}
   end
