aaad97633ed317126a2fdb7f42c4c75d5a22cb4b
Nad Flores <nflores@appsource.biz>
2014-09-09 17:30:19 +0800

    unfinish subproject test

---
+++ spec/factories/subproject_factory.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+
+  factory :subproject do
+  end
+end
--- spec/models/subproject_spec.rb
+++ spec/models/subproject_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Subproject do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "VALIDATION" do
+    let(:empty_subproject) { Subproject.create }
+
+    describe 'present attirbutes' do
+      %i(region_id municipality_id province_id barangay_id title cycle).each do |attr|
+        it { expect(empty_subproject.errors.messages[attr].first).to eq("can't be blank")}
+      end
+    end
+
+    describe "#total_tranch_must_equal_to_grant_amount validation" do
+    end
+
+  end
 end
