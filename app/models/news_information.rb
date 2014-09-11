6418569e15e03a79112b9f2337991b2f7426e884
Reynan Albaredo <chezca1228@yahoo.com>
2014-09-11 17:21:29 +0800

    configuration of paperclip in model

--- app/models/news_information.rb
+++ app/models/news_information.rb
@@ -3,7 +3,12 @@ class NewsInformation < ActiveRecord::Base
   tracked owner: ->(controller, model) { controller.current_user }
   
   #### paperclip
-  has_attached_file :news_image, :styles => { :medium => "220x220>" }
+  if ENV['PAPERCLIP_PATH'] == 1
+    has_attached_file :news_image, :styles => { :medium => "220x220>" }
+  else
+    has_attached_file :news_image, :styles => { :medium => "220x220>" },
+    :path => "/home/azureuser/shared/public/system/:class/:attachment/:id/:style/:basename.:extension"
+  end
   validates_attachment_content_type :news_image, :content_type => /\Aimage\/.*\Z/
   ####
 
