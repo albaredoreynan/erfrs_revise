e21bad321ba329211f2e1c3ee99ed66e5d5a8da5
Nad Flores <nflores@appsource.biz>
2014-09-12 14:17:17 +0800

    add paperclip script for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,6 +44,15 @@ namespace :deploy do
     end
   end
 
+  desc "build missing paperclip styles"
+  task :build_missing_paperclip_styles do
+    on roles(:app) do
+      execute "cd #{current_path}; RAILS_ENV=staging bundle exec rake paperclip:refresh:missing_styles"
+    end
+  end
+
+  after("deploy:compile_assets", "deploy:build_missing_paperclip_styles")
+
   after :publishing, :restart
 
   after :restart, :clear_cache do
