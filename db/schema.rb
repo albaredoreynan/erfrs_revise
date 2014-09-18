bf6e74b85bd92453bbdbaeb1504be42bfd84457d
Nad Flores <nflores@appsource.biz>
2014-09-18 11:37:30 +0800

    add total_grant_allocation column to municipalities table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140910082933) do
+ActiveRecord::Schema.define(version: 20140918033205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20140910082933) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "designation_type"
   end
 
   create_table "fund_allocations", force: true do |t|
@@ -100,6 +101,7 @@ ActiveRecord::Schema.define(version: 20140910082933) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "group_id"
+    t.decimal  "total_grant_allocation", precision: 15, scale: 2
   end
 
   add_index "municipalities", ["group_id"], name: "index_municipalities_on_group_id", using: :btree
@@ -337,5 +339,16 @@ ActiveRecord::Schema.define(version: 20140910082933) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
+
+  create_table "versions", force: true do |t|
+    t.string   "item_type",  null: false
+    t.integer  "item_id",    null: false
+    t.string   "event",      null: false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
 
 end
