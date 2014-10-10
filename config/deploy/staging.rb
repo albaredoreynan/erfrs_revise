1ec37c585f47979222612ef154fccafd0de705f2
Reynan Albaredo <chezca1228@yahoo.com>
2014-10-10 14:15:29 +0800

    deploying and configuring nginx

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,12 +2,12 @@ set :rails_env, :staging
 set :branch, :staging
 set :deploy_to, '/home/ubuntu'
 set :user, :ubuntu
-set :nginx_server_name, '54.169.9.32'
+set :nginx_server_name, 'erfrs.dswd.gov.ph'
 set :default_env, { 'ERFRS_USES_POSTGRESQL' => 1 } 
 set :linked_files, %w[config/database.yml tmp/pids/unicorn.pid log/unicorn.log]
 set :linked_dirs, %w{ public/paperclip }
 
-server '54.169.9.32', user: 'ubuntu', roles: %w[web app]
+server 'erfrs.dswd.gov.ph', user: 'ubuntu', roles: %w[web app]
 
 
 set :ssh_options, {
