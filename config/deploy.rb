a97441addee028038696ba2387c8c3708161a391
reynan albaredo <chezca1228@yahoo.com>
2016-03-29 10:05:34 +0800

    added files for configuration in deploying erfrs

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'erfrs'
-set :repo_url, 'git@bitbucket.org:erfrs/erfrs.git'
+set :repo_url, 'https://github.com/albaredoreynan/erfrs_revise.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
